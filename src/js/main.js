bfb57f9bda4f4f0cdda74f54328b0d709d902e4a
Segura <segura-deluna@mail.ru>
2022-08-20 23:30:20 +0500

    add logics from tabs&exchange-rates

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-import "./_vendor";
-import vars from "./_vars";
-import "./_functions";
-import "./_components";
-import "./components/_navigation";
-import "./components/_modal";
+import './_vendor';
+import vars from './_vars';
+import './_functions';
+import './_components';
+import './components/_navigation';
+import './components/_modal';
+import './components/_tabs';
+import './components/_exchange-rates';
